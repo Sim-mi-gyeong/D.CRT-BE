1492bb10aef05239e81c2e56097eaeb8fe6296be
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-11-17 02:00:04 +0900

    init : 프로젝트 세팅 및 gitignore 추가

---
+++ src/main/java/com/dcrt/DcrtApplication.java
@@ -0,0 +1,13 @@
+package com.dcrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcrtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcrt/DcrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
